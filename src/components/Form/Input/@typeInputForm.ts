1f1cb5d81a3775bf1c69d7342f2f8088438a89ef
unknown <danieleccb25@hotmail.com>
2023-03-08 16:24:39 -0300

    feat: add modal login

---
+++ src/components/Form/Input/@typeInputForm.ts
@@ -0,0 +1,8 @@
+export interface IInputForm {
+    label: string;
+    type?: string;
+    inputId: string;
+    error: string | undefined;
+    placeholder: string;
+
+}
---
+++ src/components/Form/Input/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyeldInput = styled.input`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
